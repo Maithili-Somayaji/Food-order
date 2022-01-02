6948081e77c49f873aec5b030c67ed2923e5faad
Maithili <maithilisomayaji@gmail.com>
2022-01-02 20:27:50 +0530

    Initial Commit

---
+++ admin/partials/footer.php
@@ -0,0 +1,10 @@
+<!--Footer section starts-->
+<div class="footer">
+            <div class="wrapper">
+            <p class="text-center">2021 All rights reserved,Developed By - <a href="#">Maithili and Rithu</a></p>
+            </div>
+            
+        </div>
+        <!--Footer section ends-->
+    </body>
+</html>
